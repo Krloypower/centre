e301062191dcb8ba78b14ba1e97a6c169286c401
ouyangkang <542124953@qq.com>
2021-02-26 15:09:36 +0800

    leetcode 代码提交

--- leetcode/src/main/java/first/leetcode/editor/cn/code_102.java
+++ leetcode/src/main/java/first/leetcode/editor/cn/code_102.java
@@ -27,7 +27,10 @@
 
 package first.leetcode.editor.cn;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class code_102 {
     public static void main(String[] args) {
@@ -52,7 +55,31 @@ public class code_102 {
      */
     class Solution {
         public List<List<Integer>> levelOrder(TreeNode root) {
-            return null;
+            List<List<Integer>> res = new ArrayList<>();
+
+            if (root == null){
+                return res;
+            }
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+
+
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                List<Integer> temp = new ArrayList<>();
+                while (size-- > 0) {
+                    TreeNode node = queue.poll();
+                    temp.add(node.val);
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                }
+                res.add(temp);
+            }
+            return res;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
